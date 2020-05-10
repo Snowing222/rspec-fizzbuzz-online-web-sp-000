d6c5394d06497885b24ae8ff096c6015a4f9c02f
elsa chen <snowing222@gmail.com>
2020-05-10 10:07:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
+#def fizzbuzz(int)
   if int%3==0
     if int%5==0
       "FizzBuzz"
@@ -16,3 +16,21 @@ def fizzbuzz(int)
     nil
   end
 end
+
+def fizzbuzz(int)
+  string=""
+  if int%3==0
+    string+="Fizz"
+
+  end
+
+  if int%5==0
+    string+="Buzz"
+
+  end
+
+  string
+  
+
+end
+
